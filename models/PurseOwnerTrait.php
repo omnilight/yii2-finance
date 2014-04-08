92590e9e0dc7e187386bac4e5d37dab8b5445ec9
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-04-08 14:32:15 +0400

    Fixes

---
+++ models/PurseOwnerTrait.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 08.04.14
+ * Time: 14:17
+ */
+
+namespace yz\finance\models;
+
+
+use yii\db\ActiveRecord;
+
+/**
+ * Class PurseOwnerTrait
+ * @property Purse $purse
+ * @package yz\finance\models
+ */
+trait PurseOwnerTrait
+{
+    protected function createPurse($title)
+    {
+        /**
+         * @var ActiveRecord $this
+         */
+        $purse = new Purse();
+        $purse->title = $title;
+        $purse->owner_type = self::className();
+        $purse->owner_id = $this->getPrimaryKey(false);
+
+        $purse->save();
+    }
+
+    protected function deletePurse()
+    {
+        Purse::deleteAll("owner_id = :id and owner_type = :type", [
+            ':id' => $this->getPrimaryKey(false),
+            ':type' => self::className(),
+        ]);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPurse()
+    {
+        /**
+         * @var ActiveRecord $this
+         */
+        return $this->hasOne(Purse::className(), ['owner_id' => 'id'])
+            ->where('owner_type = :type', [':type' => self::className()]);
+    }
+} 
